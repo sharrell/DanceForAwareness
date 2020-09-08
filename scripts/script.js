7e234b62e2b1edf291f8fac099e0068c088b9f07
samuel.harrell <samuel.harrell@nbcuni.com>
2020-09-08 14:01:35 -0400

    submission candidate

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-
-const NativeUI = require('NativeUI');
-
-const picker = NativeUI.picker;
-
-picker.selectedIndex.monitor().subscribe(function(index) { 
-   // index.newValue // The new selectedIndex value
-});
